5616ea9b69376e2e5022d6463b53610a7f53f1d1
ErnandesAJr <eandesjunior@hotmail.com>
2020-10-27 11:28:58 -0300

    update

--- src/services/WaterQuality.js
+++ src/services/WaterQuality.js
@@ -7,14 +7,14 @@ class WaterQuality {
   listenUserData(service) {
     return this.server.post('/chemsen/readings', (req, res) => {
       console.log(`Received HTTP message: ${JSON.stringify(req.body)}`);
-      const errors = this.checkInvalidData(req.body, ['timestamp', 'temperature', 'location', 'device']);
+      const errors = this.checkInvalidData(req.body, ['timestamp', 'data', 'device']);
 
       if (errors.length > 0)
         return res.status(400).json({ 'messages': errors });
 
       const deviceId = req.body.device;
       const timestamp = req.body.timestamp ? req.body.timestamp : new Date().getTime();
-      const payload = { "temperature": req.body.temperature, "location": req.body.location }
+      const payload = { "data": req.body.temperature }
       service.handleDeviceMessage(timestamp, payload, deviceId);
       return res.status(204).send();
     });
